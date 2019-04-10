68fb7d2d4c3951d0d4e20502338f08d46321593a
AB Rao <anirudhrao24@gmail.com>
2019-04-10 02:21:10 +0200

    integrated google cloud and firebase authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,19 @@
 import React, { Component } from 'react';
-import '@progress/kendo-theme-default/dist/all.css';
+import '@progress/kendo-theme-bootstrap/dist/all.css';
+import { BrowserRouter } from 'react-router-dom';
 
 // Import the Grid component.
 import { Grid, GridColumn } from '@progress/kendo-react-grid';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      data: [
-        {ProductName: "Chai", Price: 10},
-        {ProductName: "Chang", Price: 20}
-      ],
-      title: "Hello from KendoReact!"
-    }
-  }
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
-        <header className="App-header">
-          <img src={"#"} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.state.title}</h1>
-        </header>
-          <Grid data={this.state.data}>
-            <GridColumn field="ProductName" title="Product name"/>
-            <GridColumn field="Price"/>
-          </Grid>
+        <h1>Work in progress</h1>
       </div>
+      </BrowserRouter>
     );
   }
 }
